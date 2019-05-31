d5c43a8bb0d59c584a53aba2256c82551320342b
ptrsen <ptrsenst@gmail.com>
2019-05-30 18:47:01 -0700

    updating install.sh Ns3.29, Docker 18.09

--- install.sh
+++ install.sh
@@ -1,44 +1,65 @@
 #!/bin/bash
 
-# This script install all the required packages for The NS3DockerEmulator  https://github.com/chepeftw/NS3DockerEmulator 
-# Ns3.26 and Docker 17.06
-# To running , open Terminal and execute  
+# This script install all the required packages for The NS3DockerEmulator  https://github.com/chepeftw/NS3DockerEmulator
+# Ns3.29 and Docker 18.09
+# To run , open Terminal and execute
 # source install.sh
 
-echo -e "\n\n Updating enviroment... \n" 
-sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get dist-upgrade
+echo -e "\n\n Updating enviroment... \n"
+sudo apt update
+sudo apt -y dist-upgrade
 
-echo -e "\n\n Installing git ... \n" 
-sudo apt-get install git
+# https://www.nsnam.org/wiki/Installation#Ubuntu.2FDebian.2FMint
 
-echo -e "\n\n Installing Ns3 required packages ... \n" 
-sudo apt-get install gcc g++ python python-dev mercurial bzr gdb valgrind gsl-bin libgsl2 libgsl2:i386 flex bison tcpdump sqlite sqlite3 libsqlite3-dev libxml2 libxml2-dev libgtk2.0-0 libgtk2.0-dev uncrustify doxygen graphviz imagemagick python-pygraphviz python-kiwi python-pygoocanvas libgoocanvas-dev python-pygccxml cmake autoconf libc6-dev libc6-dev-i386 g++-multilib texlive texlive-extra-utils texlive-latex-extra texlive-font-utils texlive-lang-portuguese dvipng python-pygraphviz python-kiwi python-pygoocanvas libgoocanvas-dev ipython libboost-signals-dev libboost-filesystem-dev openmpi-bin openmpi-common openmpi-doc libopenmpi-dev qt4-dev-tools libqt4-dev unzip p7zip-full unrar-free cvs 
-sudo apt-get install python-pip
-pip install pygccxml
-pip install --upgrade pip
-sudo -H pip install pygccxml --upgrade
+echo -e "\n\n Installing tools for Ns3 ... \n"
+sudo apt -y install gcc g++ python python3 python-pip python3-pip make cmake tar unzip patch p7zip-full unrar-free
+sudo apt -y install python-dev python3-dev python-setuptools python3-setuptools git mercurial qt5-default net-tools
+sudo apt -y install gir1.2-goocanvas-2.0 python-gi python-gi-cairo python-pygraphviz python-kiwi python3-gi python3-gi-cairo python3-pygraphviz gir1.2-gtk-3.0 ipython ipython3
+sudo apt -y install openmpi-bin openmpi-common openmpi-doc libopenmpi-dev
+sudo apt -y install autoconf cvs bzr unrar
+sudo apt -y install gdb valgrind
+sudo apt -y install uncrustify
+sudo apt -y install doxygen graphviz imagemagick
+sudo apt -y install texlive texlive-extra-utils texlive-latex-extra texlive-font-utils texlive-lang-portuguese dvipng latexmk
+sudo apt -y install python-sphinx dia
+sudo apt -y install gsl-bin libgslcblas0:i386 libgslcblas0 libgsl-dev
+sudo apt -y install flex bison libfl-dev
+sudo apt -y install tcpdump
 
-echo -e "\n\n Setting Ns3 workspace ... \n" 
+sudo apt -y install sqlite sqlite3 libsqlite3-dev
+sudo apt -y install libxml2 libxml2-dev
+sudo apt -y install libc6-dev libc6-dev-i386 libclang-dev llvm-dev automake
+sudo apt -y install libgtk2.0-0 libgtk2.0-dev
+pip install cxxfilt
+pip3 install cxxfilt
+sudo apt -y install vtun lxc
+sudo apt -y install libboost-signals-dev libboost-filesystem-dev
+
+# https://www.nsnam.org/wiki/Installation#Installation
+
+echo -e "\n\n Setting Ns3 workspace ... \n"
+echo -e "\n\n Installing and Setting bake tool ... \n"
 mkdir Ns3
 cd Ns3
 hg clone http://code.nsnam.org/bake
 cd bake
 
- 
 echo "export BAKE_HOME=$PWD" >> ~/.bashrc
 echo "export PATH=$PATH:$BAKE_HOME:$BAKE_HOME/build/bin" >> ~/.bashrc
 echo "export PYTHONPATH=$PYTHONPATH:$BAKE_HOME:$BAKE_HOME/build/lib" >> ~/.bashrc
 source ~/.bashrc
-
+echo -e "\n\n Verifying bake and missing packages to start Ns3 installation  ... \n"
 python $BAKE_HOME/bake.py check
-python $BAKE_HOME/bake.py configure -e ns-3.26
+# rm -rf bakefile.xml
+# python3 $BAKE_HOME/bake.py configure -e ns-3.29
+python $BAKE_HOME/bake.py configure -e ns-allinone-3.29
+python $BAKE_HOME/bake.py show
+echo -e "\n\n Downloading and building Ns3   ... \n"
 python $BAKE_HOME/bake.py download
 python $BAKE_HOME/bake.py build
 
-echo -e "\n\n Verifying Ns3  ... \n" 
-cd source/ns-3.26
+echo -e "\n\n Verifying Ns3  ... \n"
+cd source/ns-3.29
 ./waf
 
 echo -e "\n\n Recompoling NS3 in optimized mode  ... \n"
@@ -51,6 +72,7 @@ cp examples/tutorial/first.cc scratch/
 ./waf
 ./waf --run scratch/first
 cd ~
+
 
 echo -e "\n\n Installing Docker required packages  ... \n"
 
@@ -75,3 +97,27 @@ sudo apt install uml-utilities
 
 git clone https://github.com/chepeftw/NS3DockerEmulator.git
 cd NS3DockerEmulator
+
+# https://docs.docker.com/install/linux/docker-ce/ubuntu/#set-up-the-repository
+
+echo -e "\n\n Installing Docker required packages  ... \n"
+sudo apt -y install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
+service lxcfs stop
+sudo apt-get -y remove lxc-common lxcfs lxd lxd-client
+
+sudo apt update
+sudo apt -y dist-upgrade
+sudo apt -y install docker-ce
+
+echo -e "\n\n  Verifying  Docker  ... \n"
+sudo docker run hello-world
+
+
+
+echo -e "\n\n Installing Network Bridges  ... \n"
+sudo apt -y install bridge-utils
+sudo apt -y install uml-utilities
